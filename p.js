6aeb1e9a5746b92315da7dd0e7f2fcd446b2963e
GeetanjaliTest <giitanjalim@gmail.com>
2021-08-31 18:15:06 +0530

    Create p.js

---
+++ p.js
@@ -0,0 +1,28 @@
+function handlePlayerArrowCollision() {
+  for (var i = 0; i < playerArrows.length; i++) {
+    var baseCollision = Matter.SAT.collides(
+      playerArrows[i].body,
+      computerBase.body
+    );
+
+    var archerCollision = Matter.SAT.collides(
+      playerArrows[i].body,
+      computerArcher.body
+    );
+
+    var computerCollision = Matter.SAT.collides(
+      playerArrows[i].body,
+      computer.body
+    );
+
+    if (
+      baseCollision.collided ||
+      archerCollision.collided ||
+      computerCollision.collided
+    ) {
+      console.log("Player arrow Collided"); 
+      }
+    }
+  
+
+}
